6dc4275d9c36787e4dfa152fb9ccad0e022b2add
biranjay kumar gupta <biranjaykumargupta@gmail.com>
2025-02-03 18:23:14 +0530

    added the navbar to signup

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Navbar from "../../components/Navbar";
 
 const backendUrl = import.meta.env.VITE_SERVER;
 
